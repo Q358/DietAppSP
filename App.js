f5fd0d93edcaf061c6f932e8f29bdf4781411f4b
Q358 <mwquinn38@gmail.com>
2022-10-18 14:11:05 -0400

    Added expo barcode reader page with cancel button Also cleaned up some imports

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import Diet from './pages/Diet';
-import Home from './pages/Home';
-import Landing from './pages/Landing';
-import Login from './pages/sign in/Login';
-import SignUp from './pages/sign in/SignUp';
-import Trophies from './pages/Trophies';
-import Workout from './pages/Workout';
-//import Sandbox from './pages/sandbox';
+import { NavigationContainer, StackActions } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import BarcodeScanSceen from './pages/BarcodeScanScreen'
+import Diet from './pages/Diet'
+import Home from './pages/Home'
+import Landing from './pages/Landing'
+import Login from './pages/sign in/Login'
+import SignUp from './pages/sign in/SignUp'
+import Trophies from './pages/Trophies'
+import Workout from './pages/Workout'
 
 const Stack = createNativeStackNavigator();
 
@@ -23,17 +21,9 @@ export default function App() {
         <Stack.Screen name="Home" component={Home}/>
         <Stack.Screen name= "Trophies" component={Trophies}/>
         <Stack.Screen name="Diet" component={Diet}/>
+        <Stack.Screen name="Barcode" component={BarcodeScanSceen}/>
         <Stack.Screen name="Workout" component={Workout}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/BottomNav.js
+++ components/BottomNav.js
@@ -1,28 +1,18 @@
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { Alert, StyleSheet, TouchableOpacity, View } from "react-native";
-import { faAppleWhole } from "@fortawesome/free-solid-svg-icons";
-import { faPersonRunning } from "@fortawesome/free-solid-svg-icons";
-import { faBarcode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome"
+import { StyleSheet, TouchableOpacity, View } from "react-native"
+import { faAppleWhole } from "@fortawesome/free-solid-svg-icons"
+import { faPersonRunning } from "@fortawesome/free-solid-svg-icons"
+import { faBarcode } from "@fortawesome/free-solid-svg-icons"
 
 export default function BottomNav({ navigation }) {
-  const styles = StyleSheet.create({
-    container: {
-      flexDirection:"row",
-      marginBottom: 15
-    },
-    navButton: {
-      marginVertical:20,
-      marginHorizontal:50
-      //marginHorizontal:65
-    }
-  })
+
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.navButton} onPress={()=>navigation.navigate("Diet")}>
         <FontAwesomeIcon icon={faAppleWhole} size={50}/>
       </TouchableOpacity>
-      <TouchableOpacity style={{borderRadius:50, backgroundColor:"white", padding:15, height:80}} onPress={()=>Alert.alert("Barcode Reader!")}>
-        <FontAwesomeIcon icon={faBarcode} size={50}/>
+      <TouchableOpacity style={{marginTop:5, borderRadius:50, backgroundColor:"white", padding:15, height:80}} onPress={()=>navigation.navigate("Barcode")}>
+        <FontAwesomeIcon icon={faBarcode} size={50} />
       </TouchableOpacity>
       <TouchableOpacity style={styles.navButton} onPress={()=>navigation.navigate("Workout")}>
         <FontAwesomeIcon icon={faPersonRunning} size={50}/>
@@ -30,3 +20,16 @@ export default function BottomNav({ navigation }) {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection:"row",
+      marginBottom: 15,
+      backgroundColor:"orange",
+      borderRadius:20
+    },
+    navButton: {
+      marginVertical:20,
+      marginHorizontal:30
+    }
+  })
