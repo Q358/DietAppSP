7ee7bc728debf751c7508ce4b8b59724654cdcec
attorri <attorri@cua.edu>
2022-10-25 12:26:19 -0400

    reloaded diet section of home page

--- pages/Home.js
+++ pages/Home.js
@@ -1,53 +1,33 @@
 import React, {useState, useEffect} from 'react';
-import { SafeAreaView, Image, StyleSheet, Text, TouchableOpacity, View, Dimensions, ScrollView } from 'react-native';
+import { SafeAreaView, Button, Image, StyleSheet, Text, TouchableOpacity, View, Dimensions } from 'react-native';
 import { useFonts } from 'expo-font';
 import { faTrophy, faUserGroup, faAppleWhole, faFish, faCarrot, faBreadSlice, faWineGlass, faCheese, faCookieBite, faSmoking } from '@fortawesome/free-solid-svg-icons';
 import BottomNav from "../components/BottomNav";
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { Avatar, LinearProgress } from "@rneui/themed";
-import profilePic from "../assets/favicon.png"
+import { Divider, LinearProgress } from "@rneui/themed";
 
 export default function Home({ navigation }) {
   const [loaded] = useFonts({
     AdidogDemo: require('../assets/fonts/AdidogDemo-RpqMo.otf'),
   });
-
+  // const [count, setCount] = useState(0);
+  // const onPress = () => setCount(prevCount => prevCount + 1);
   if (!loaded) {
     return null;
   }
   const windowWidth = Dimensions.get("window").width;
   const windowHeight = Dimensions.get('window').height;
   var progress = 0.76
-
   return (
+
     <View style={{...styles.container, justifyContent:"space-between", alignItems: 'center'}}>
-        <View style={{flexDirection:"row", marginTop:50, width:"100%"}}>
-          <TouchableOpacity style = {styles.friendsButton} onPress={()=>navigation.navigate("Diet")}>
-            <FontAwesomeIcon icon = {faUserGroup} size = {20} color ={'white'}/> 
-          </TouchableOpacity>
-          <TouchableOpacity style = {styles.trophyButton} onPress={()=>navigation.navigate("Trophies")}>
-            <FontAwesomeIcon icon = {faTrophy} size = {20} color ={'white'}/> 
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
-            <Avatar style={{marginLeft:210, width:40,height:40, border:"solid 1px black", borderBottomColor:"black"}} size={"large"} rounded source={profilePic}/>
-          </TouchableOpacity>
-        </View>
-        <Text style = {{fontFamily: "AdidogDemo", color:"white", fontSize:20, marginVertical:20}}>Happy Saturday,</Text>
-        <TouchableOpacity style = {styles.weeklyProgressButton} onPress = {() => navigation.navigate("Diet")}>
-          <Text style = {styles.boxText}>Weekly Goal Progress </Text>
-          <Text style={{marginLeft:5, marginBottom:30}}>On track - keep it up!</Text>
-          <Text style={{fontSize:20, marginBottom:5}}>{progress * 100}%</Text>
-          <LinearProgress value={progress} variant="determinate" />
+      <View style={styles.container}>
+      <View style={{flexDirection:"row", marginTop:50, width:"100%"}}>
+        <TouchableOpacity style = {styles.friendsButton} onPress={()=>navigation.navigate("Diet")}>
+          <FontAwesomeIcon icon = {faUserGroup} size = {20} color ={'white'}/> 
         </TouchableOpacity>
-        <View style = {{...styles.dividerStyle, marginHorizontal:windowWidth/9, marginVertical:windowHeight/45, }}/>
-        <Text style = {{fontFamily: "AdidogDemo", color:"white", fontSize:12, marginBottom:5}}>Daily Breakdown</Text>
-        <TouchableOpacity style = {styles.weeklyProgressButton} onPress = {() => navigation.navigate("Diet")}>
-          <Text style = {styles.boxText}>Diet</Text>
-          <View></View>
-        </TouchableOpacity>
-        <TouchableOpacity style = {{...styles.weeklyProgressButton, marginTop:10}} onPress = {() => navigation.navigate("Diet")}>
-          <Text style = {styles.boxText}>Workout</Text>
-          <View></View>
+        <TouchableOpacity style = {styles.trophyButton} onPress={()=>navigation.navigate("Trophies")}>
+          <FontAwesomeIcon icon = {faTrophy} size = {20} color ={'white'}/> 
         </TouchableOpacity>
       </View>
       <Text style = {{fontFamily: "AdidogDemo", color:"white", fontSize:20, marginVertical:20}}>Happy Saturday,</Text>
@@ -137,10 +117,12 @@ const styles = StyleSheet.create({
       //marginHorizontal :16
     },
     dividerStyle: {
-      borderBottomColor : "lightgray", 
-      borderBottomWidth:10, 
-      borderRadius:30, 
-      width:250
+      width : 250,
+      marginVertical: 25,
+      //marginHorizontal: 20,
+      borderRadius:30,
+      color: 'white',
+      borderColor: 'white'
     },
     friendsButton:{
       backgroundColor: '#0F2135',
