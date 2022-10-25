060f10b3eaef9d6634c585f83c8b279f65810865
Geetanjali Sharma <77789166+Geetanjali1481@users.noreply.github.com>
2022-10-25 11:33:40 -0400

    Update Diet.js

--- pages/Diet.js
+++ pages/Diet.js
@@ -1,11 +1,15 @@
 import { SafeAreaView, Image, StyleSheet, TouchableOpacity, Text, View, Dimensions, ScrollView } from "react-native";
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faAngleDown, faAngleLeft, faTrophy } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function Diet({navigation}) {
   return (
     <View style={{...styles.container, alignItems: "center"}}>
-      <Text style = {{marginVertical:50, marginTop:-400}}>Diet</Text>
+      <Text style = {{marginVertical:50, marginTop:-400, color:'white', fontFamily:'AdidogDemo', fontSize:10}}>Diet</Text>
+      <View style = {{...styles.leftArrowIcon, marginRight: 300}}>
+      <FontAwesomeIcon icon={faAngleLeft} size = {30} color = {'grey'}/>
+      </View>
       <Text style = {{fontFamily: "BandarBold", color:"white", fontSize:40, marginTop:-15, marginBottom: 10}}>Today's Meals</Text>
       <View style = {{...styles.rectangle, marginTop: 10, marginRight:250}}/>
       <View style = {{...styles.rectangle, marginTop: -100, alignItems:"center"}}/>
@@ -31,6 +35,13 @@ const styles = StyleSheet.create({
       backgroundColor: '#333333',
       borderRadius:10
     },
+
+    leftArrowIcon: {
+      marginTop: -80, 
+      marginBottom: 40
+      
+
+    }
 
     // dividerStyle: {
     //   borderBottomColor : "lightgray", 
