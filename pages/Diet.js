a34a1f2fdd96e3af8289462ec5dc2ca199c54ac6
Geetanjali Sharma <77789166+Geetanjali1481@users.noreply.github.com>
2022-10-24 22:03:05 -0400

    Update Diet.js

--- pages/Diet.js
+++ pages/Diet.js
@@ -1,11 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, Image, StyleSheet, TouchableOpacity, Text, View, Dimensions, ScrollView } from "react-native";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import BottomNav from "../components/BottomNav";
+import React from "react";
 
-export default function Diet() {
+export default function Diet({navigation}) {
   return (
     <View style={{...styles.container, justifyContent:"space-between", alignItems: "center"}}>
       <Text style = {{marginVertical:50}}>Diet</Text>
-      <Text style = {{fontFamily: "AdidogDemo", color:"white", fontSize:20, marginBottom:750, marginRight:70, marginTop:-15}}>Today's Meal</Text>
-
+      <Text style = {{fontFamily: "BandarBold", color:"white", fontSize:40, marginBottom:750, marginTop:-15}}>Today's Meal</Text>
+      {/* <View style = {{...styles.rectangle, alignItems: "center"}}><Text style = {{marginVertical:50}}>Test</Text></View> */}
+      <View style = {{height:50, width:50, backgroundColor:'red'}}/>
     </View>
   )
 }
@@ -17,4 +21,18 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+
+    // rectangle: {
+    //   height: 70,
+    //   width: 70,
+    //   backgroundColor: '#333333'
+    // },
+
+    // dividerStyle: {
+    //   borderBottomColor : "lightgray", 
+    //   borderBottomWidth:10, 
+    //   borderRadius:30, 
+    //   width:250
+    // },
 });
+
