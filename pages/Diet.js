5c47ac9ece8fafb0254139ecdf347c808b7615d9
Geetanjali Sharma <77789166+Geetanjali1481@users.noreply.github.com>
2022-10-26 16:31:22 -0400

    Update Diet.js

--- pages/Diet.js
+++ pages/Diet.js
@@ -1,12 +1,12 @@
 import { SafeAreaView, Image, StyleSheet, TouchableOpacity, Text, View, Dimensions, ScrollView } from "react-native";
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faAngleDown, faAngleLeft, faAngleRight, faAppleWhole, faTrophy } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown, faAngleLeft, faAngleRight, faAppleWhole, faBarcode, faChartColumn, faHouse, faTrophy } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function Diet({navigation}) {
   return (
     <View style={{...styles.container, alignItems: "center"}}>
-      <Text style = {{marginVertical:50, marginTop:-200, color:'white', fontFamily:'AdidogDemo', fontSize:10}}>Diet</Text>
+      <Text style = {{marginVertical:50, marginTop:40, color:'white', fontFamily:'AdidogDemo', fontSize:10}}>Diet</Text>
       <View style = {{...styles.leftArrowIcon, marginRight: 300}}>
         <TouchableOpacity onPress={()=>navigation.navigate("Home")}>
           <FontAwesomeIcon icon={faAngleLeft} size = {30} color = {'grey'}/>
@@ -29,6 +29,19 @@ export default function Diet({navigation}) {
         <FontAwesomeIcon icon={faAngleRight} size = {30} color = {'white'}/>
         </View>
       </View>
+    <Text style = {{fontFamily: "BandarBold", color:"white", fontSize:35, marginTop: 20}}>Cheats</Text>
+    <View style = {{...styles.rectangle3, marginTop: 10, marginRight:5}}/>
+    <TouchableOpacity onPress={()=>navigation.navigate("Home")}>
+    <View style = {{...styles.houseButton, marginRight:200, marginTop: 50}}>
+      <FontAwesomeIcon icon={faHouse} size = {40} color = {'white'}/>
+    </View>
+    </TouchableOpacity>
+    <TouchableOpacity style={{marginTop:-65, borderRadius:50, backgroundColor:"white", padding:15, height:80}} onPress={()=>navigation.navigate("Barcode")}>
+        <FontAwesomeIcon icon={faBarcode} size={50} />
+      </TouchableOpacity>
+    <View style = {{...styles.chartButton, marginLeft:200, marginTop:-50}}>
+      <FontAwesomeIcon icon={faChartColumn} size = {40} color = {'white'}/>
+    </View>
     </View>
   )
 }
@@ -59,7 +72,21 @@ const styles = StyleSheet.create({
       borderRadius:30, 
       marginLeft: 250
 
-    }
+    },
+
+    smallRectangle: {
+      height: 50, 
+      width: 60, 
+      color: 'red'
+    }, 
+
+    rectangle3: {
+      height: 75,
+      width: 350,
+      backgroundColor: '#333333',
+      borderRadius:10
+    },
+
 
 });
 
