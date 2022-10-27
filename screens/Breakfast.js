076649ad1bc1cf2b940a1bddbab84c56f21ea6d5
Geetanjali Sharma <77789166+Geetanjali1481@users.noreply.github.com>
2022-10-27 15:37:24 -0400

    Create Breakfast.js

---
+++ screens/Breakfast.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Breakfast({navigation}) {
+    return (
+      <View style={styles.container}>
+        <Text>Breakfast</Text>
+      </View>
+    )
+  }
+  
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+  });
