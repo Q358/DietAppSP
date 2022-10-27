4f6c9f3ff291b0bc5876b6a2881e32272cc853ab
Geetanjali Sharma <77789166+Geetanjali1481@users.noreply.github.com>
2022-10-27 17:42:15 -0400

    Update Diet.js

--- screens/Diet.js
+++ screens/Diet.js
@@ -2,8 +2,9 @@ import { SafeAreaView, Image, StyleSheet, TouchableOpacity, Text, View, Dimensio
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faAngleDown, faAngleLeft, faAngleRight, faAppleWhole, faBarcode, faChartColumn, faHouse, faTrophy } from "@fortawesome/free-solid-svg-icons";
 import FoodBlock from "../components/FoodBlock"
+import React from "react";
 
-export default function Diet({navigation}) {
+export default function Diet({ navigation }) {
   return (
     <View style={{...styles.container, alignItems: "center"}}>
       <Text style = {{marginVertical:50, marginTop:30, color:'white', fontFamily:'UbuntuBold', fontSize:20}}>Diet</Text>
@@ -25,7 +26,7 @@ export default function Diet({navigation}) {
       <Text style = {{marginVertical:50, marginTop: 10, marginRight:260}}>Breakfast</Text>
       <Text style = {{marginVertical:50, marginTop: -70, alignItems: "center"}}>Lunch</Text>
       <Text style = {{marginVertical:50, marginTop: -70, marginLeft:260}}>Dinner</Text>
-      <Text style = {{fontFamily: "BandarBold", color:"white", fontSize:40, marginTop:-15, marginBottom: 10, marginRight:80}}>This Week</Text>
+      <Text style = {{fontFamily: "BandarBold", color:"white", fontSize:40, marginTop:-25, marginBottom: 10, marginRight:80}}>This Week</Text>
       <View style = {{...styles.rectangle2, marginTop: 10, marginRight:250}}>
         <Text style = {{marginTop:20, marginLeft: 135, color:"white", fontFamily:"AdidogDemo", fontSize:10}}>Sunday</Text>
         <View style = {{...styles.leftArrow, marginTop:30}}>
