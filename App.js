50db1db2b2160bf7f28e53670b12cfca7a514141
Geetanjali Sharma <77789166+Geetanjali1481@users.noreply.github.com>
2022-11-14 15:14:31 -0500

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { registerRootComponent } from 'expo'
 import { AuthProvider } from './config/authContext'
 import BarcodeScanSceen from './screens/BarcodeScanScreen'
 import Diet from './screens/Diet'
+import Friends from './screens/Friends'
 import Home from './screens/Home'
 import Landing from './screens/Landing'
 import LoadingScreen from './screens/LoadingScreen'
@@ -14,30 +15,75 @@ import SignUp from './screens/signIn/SignUp'
 import Trophies from './screens/Trophies'
 import Workout from './screens/Workout'
 import Breakfast from './screens/Breakfast'
+import { createTheme, ThemeConsumer, ThemeProvider, useTheme } from '@rneui/themed'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useColorScheme } from 'react-native'
+import { useEffect, useState } from "react"
 
 
 const Stack = createNativeStackNavigator();
 // registerRootComponent(App);
 
 export default function App() {
+  const [mode, setMode] = useState()
+  const colorScheme = useColorScheme()
+
+  useEffect(() => {
+
+    const initMode = async () => {
+      await AsyncStorage.getItem("appTheme").then(value => setMode(JSON.parse(value)))
+      mode ? null : colorScheme ? setMode(colorScheme) : setMode("light")
+    }
+    initMode()
+    
+  }, [])
+
+  const mainTheme = createTheme({
+    lightColors: {
+      primary: 'lightgreen', // #68CC68?
+      secondary: 'white',
+      tertiary: "#29a442",
+      textPrimary: "white",
+      textSecondary:"black"
+    },
+    darkColors: {
+      primary: '#3c3c3c',
+      secondary: '#013220',
+      tertiary: '#1a2421',
+      textPrimary:"white",
+      textSecondary:"white"
+    },
+    mode: mode,
+  });
+
+  if(!mode){
+    return null
+  }
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false,}}>
-          <Stack.Screen name="Loading" component={LoadingScreen}/>
-          <Stack.Screen name="Landing" component={Landing}/>
-          <Stack.Screen name="Login" component={Login}/>
-          <Stack.Screen name="SignUp" component={SignUp}/>
-          <Stack.Screen name="Home" component={Home}/>
-          <Stack.Screen name= "Trophies" component={Trophies}/>
-          <Stack.Screen name="Profile" component={Profile}/>
-          <Stack.Screen name="Diet" component={Diet}/>
-          <Stack.Screen name="Barcode" component={BarcodeScanSceen}/>
-          <Stack.Screen name="Workout" component={Workout}/>
-          <Stack.Screen name="Forgot" component={ForgotPassword}/>
-          <Stack.Screen name="Breakfast" component={Breakfast}/>
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AuthProvider>
+    <ThemeProvider theme={mainTheme}>
+      <AuthProvider>
+        <ThemeConsumer>
+          {({ theme }) => (
+          <NavigationContainer>
+            <Stack.Navigator screenOptions={{headerShown: false,}}>
+              <Stack.Screen name="Loading" component={LoadingScreen}/>
+              <Stack.Screen name="Landing" component={Landing}/>
+              <Stack.Screen name="Login" component={Login}/>
+              <Stack.Screen name="SignUp" component={SignUp}/>
+              <Stack.Screen name="Home" component={Home}/>
+              <Stack.Screen name="Friends" component={Friends}/>
+              <Stack.Screen name="Trophies" component={Trophies}/>
+              <Stack.Screen name="Profile" component={Profile}/>
+              <Stack.Screen name="Diet" component={Diet}/>
+              <Stack.Screen name="Barcode" component={BarcodeScanSceen}/>
+              <Stack.Screen name="Workout" component={Workout}/>
+              <Stack.Screen name="Forgot" component={ForgotPassword}/>
+              <Stack.Screen name="Breakfast" component={Breakfast}/>
+            </Stack.Navigator>
+          </NavigationContainer>
+          )}
+        </ThemeConsumer>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
