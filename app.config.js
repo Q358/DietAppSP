94c57f1ec1412f58bfabb450935551d257daf0c2
Geetanjali Sharma <77789166+Geetanjali1481@users.noreply.github.com>
2022-11-14 15:34:04 -0500

    Revert "Update app.config.js"
    This reverts commit 38467773f973e92bea1225815a1704979bcaa4f7, reversing
    changes made to 0b88d8f98c288ede6c92bf9c7a63d555ced75de0.

--- app.config.js
+++ app.config.js
@@ -38,9 +38,7 @@ export default {
       projectId: process.env.PROJECT_ID,
       storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
-      appId: process.env.APP_ID,
-      consumerKey: process.env.CONSUMER_KEY,
-      consumerSecret: process.env.CONSUMER_SECRET
+      appId: process.env.APP_ID
     }
   }
 };
