9e1a88fe622212fdfcad02255c3e599d91b2f664
Geetanjali Sharma <77789166+Geetanjali1481@users.noreply.github.com>
2022-11-14 15:48:44 -0500

    Create Cheats.js

---
+++ screens/Cheats.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Image, ScrollView, SafeAreaView} from "react-native";
+import React from "react";
+
+export default function Cheats({ navigation }) {
+  
+    return (
+        <Text>Cheats</Text>
+
+    )
+
+}
