9ab35cd415b2b34522c804c4d74c588bceacb292
Geetanjali Sharma <77789166+Geetanjali1481@users.noreply.github.com>
2022-11-14 15:14:55 -0500

    Update BottomNav.js

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -3,35 +3,39 @@ import { StyleSheet, TouchableOpacity, View } from "react-native"
 import { faAppleWhole } from "@fortawesome/free-solid-svg-icons"
 import { faPersonRunning } from "@fortawesome/free-solid-svg-icons"
 import { faBarcode } from "@fortawesome/free-solid-svg-icons"
+import { makeStyles } from "@rneui/themed"
 
 export default function BottomNav({ navigation }) {
+  const styles = useStyles()
 
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.navButton} onPress={()=>navigation.navigate("Diet")}>
-        <FontAwesomeIcon icon={faAppleWhole} size={50}/>
+        <FontAwesomeIcon icon={faAppleWhole} size={50} color="white"/>
       </TouchableOpacity>
       <TouchableOpacity style={{marginTop:5, borderRadius:50, backgroundColor:"white", padding:15, height:80}} onPress={()=>navigation.navigate("Barcode")}>
         <FontAwesomeIcon icon={faBarcode} size={50} />
       </TouchableOpacity>
       <TouchableOpacity style={styles.navButton} onPress={()=>navigation.navigate("Workout")}>
-        <FontAwesomeIcon icon={faPersonRunning} size={50}/>
+        <FontAwesomeIcon icon={faPersonRunning} size={50} color="white"/>
       </TouchableOpacity>
     </View>
   )
 }
 
-const styles = StyleSheet.create({
+const useStyles = makeStyles((theme) => ({
     container: {
       flexDirection:"row",
-      marginBottom: 15,
-backgroundColor:"lightgray",
- 
-      borderWidth:3,
-      borderRadius:20
+      backgroundColor: theme.colors.primary,
+      borderTopWidth:3,
+      borderColor: theme.colors.tertiary,
+      marginTop:20,
+      width:"80%",
+      justifyContent:"center",
+      paddingVertical:3
     },
     navButton: {
       marginVertical:20,
-      marginHorizontal:30
+      marginHorizontal:40
     }
-  })
+  }))
