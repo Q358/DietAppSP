0817ddb0b07fcff02659619e3f96f196f090d1bd
Christian Attorri <91044029+attorri@users.noreply.github.com>
2023-02-14 12:08:48 -0500

    Simple

--- metro.config.js
+++
@@ -1,7 +0,0 @@
-const { getDefaultConfig } = require("@expo/metro-config");
-
-const defaultConfig = getDefaultConfig(__dirname);
-// Allows it to interpret cjs files
-defaultConfig.resolver.assetExts.push("cjs");
-
-module.exports = defaultConfig;
