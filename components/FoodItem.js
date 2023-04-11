16a5b778474a999b63bb315739652d0d50e0a5e6
Q358 <mwquinn38@gmail.com>
2023-04-11 11:37:16 -0400

    Fixed BackButton color issue

--- components/FoodItem.js
+++ components/FoodItem.js
@@ -1,5 +1,6 @@
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { useTheme } from "@rneui/themed";
 import { Text, View, TouchableOpacity } from "react-native";
 
 // Should this file be renamed?
@@ -19,13 +20,14 @@ export default function FoodItem({ icon, color, meal, portion, backColor }) {
 }
 
 export function BackButton({ navigation, title }){
+  const { theme } = useTheme()
   return(
     <View style={{flexDirection:"row", marginTop: 75, alignItems:"center", justifyContent:"space-between", width:"80%", marginBottom: 10}}>
       <TouchableOpacity onPress={()=>navigation.goBack()}>
         <FontAwesomeIcon icon={faAngleLeft} size = {30} color = {'grey'}/>
       </TouchableOpacity>
       <Text style = {{textAlign: "center", color:'white', fontFamily:'fontBold', fontSize:30}}>{title}</Text>
-      <FontAwesomeIcon icon={faAngleLeft} size = {30} color = {"lightgreen"}/>
+      <FontAwesomeIcon icon={faAngleLeft} size = {30} color = {theme.colors.primary}/>
     </View>
   )
 }
